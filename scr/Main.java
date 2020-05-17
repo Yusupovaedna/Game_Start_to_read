cbd6fa10102a07920580e7b0ac225a81e83af533
Edna_ <yusupovaedna@ya.ru>
2020-05-17 13:55:00 +0000

    Upload New File

---
+++ scr/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import javax.swing.*;
+public class Main {
+
+    public static void main(String[] args){
+        JFrame f = new JFrame ("Start to read");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1100,600);
+        f.setVisible(true);
+
+    }
+}
