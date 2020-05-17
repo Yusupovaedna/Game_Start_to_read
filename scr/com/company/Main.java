80ff9c96ec2a45883e4c2ff8e6577a911a602b40
Edna_ <yusupovaedna@ya.ru>
2020-05-17 21:26:44 +0300

    Update start-to-read-in-english.java

--- scr/com/company/Main.java
+++ scr/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
         JFrame f = new JFrame ("Start to read");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(1100,600);
+        f.setSize(1300,800);
         f.add(new Sea());
         f.setVisible(true);
 
--- scr/com/company/Player.java
+++ scr/com/company/Player.java
@@ -7,23 +7,34 @@ import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 
 public class Player  {
+    public static final int MAX_V =30;
+    public static final int MAX_TOP =-10;
+    public static final int MAX_BOTTOM =400;
+
 
     Image fish = new ImageIcon("scr/resources/straight2.png").getImage();
-    int v=5;
+    int v=0;
     int dv=0;
     int s=0;
 
     int x=30;
     int y=100;
+    int dy=0;
 
     int  sea1=0;
-    int sea2=1100;
+    int sea2=1500;
 
     public void move(){
         s+=v;
+        v+=dv;
+        if (v>=MAX_V) v=MAX_V;
+        if (v<=0) v=0;
+        y-=dy;
+        if(y <= MAX_TOP)  y=MAX_TOP;
+        if(y >= MAX_BOTTOM) y=MAX_BOTTOM;
         if (sea2-v <=0){
             sea1=0;
-            sea2=1100;
+            sea2=1500;
         }else {
         sea1-=v;
         sea2-=v;
@@ -31,10 +42,24 @@ public class Player  {
     }
 
     public void keyPressed(KeyEvent e){
-        JOptionPane.showMessageDialog(null, "key pressed");
-    }
+        int key = e.getKeyCode();
+        switch (key){
+            case(KeyEvent.VK_RIGHT): dv=1;break;
+            case(KeyEvent.VK_LEFT): dv=-1;break;
+            default: dv=0;
+            case(KeyEvent.VK_UP): dy=5;break;
+            case(KeyEvent.VK_DOWN): dy=-5;break;
+        }}
     public void keyReleased(KeyEvent e){
-        JOptionPane.showMessageDialog(null, "key released");
+        int key = e.getKeyCode();
+        switch (key){
+            case(KeyEvent.VK_RIGHT  ): dv=0;break;
+            case(KeyEvent.VK_LEFT): dv =0;break;
+
+            case(KeyEvent.VK_UP): dy=0;break;
+            case(KeyEvent.VK_DOWN): dy=0;break;
+            default: dv=0;
+        }
     }
 
 
