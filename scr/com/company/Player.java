bfee400c7ae1ce7962e9162dbd372bfeeb471354
Edna_ <yusupovaedna@ya.ru>
2020-05-17 20:44:43 +0300

    Update start-to-read-in-english.java

--- scr/com/company/Player.java
+++ scr/com/company/Player.java
@@ -2,25 +2,39 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 
 public class Player  {
 
     Image fish = new ImageIcon("scr/resources/straight2.png").getImage();
-    int v=0;
+    int v=5;
     int dv=0;
     int s=0;
 
-    int x=100;
-    int y=30;
+    int x=30;
+    int y=100;
 
     int  sea1=0;
+    int sea2=1100;
 
     public void move(){
         s+=v;
+        if (sea2-v <=0){
+            sea1=0;
+            sea2=1100;
+        }else {
         sea1-=v;
+        sea2-=v;
+        }
+    }
 
+    public void keyPressed(KeyEvent e){
+        JOptionPane.showMessageDialog(null, "key pressed");
+    }
+    public void keyReleased(KeyEvent e){
+        JOptionPane.showMessageDialog(null, "key released");
     }
 
 
--- scr/com/company/Sea.java
+++ scr/com/company/Sea.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 public class Sea extends JPanel implements  ActionListener {
@@ -15,12 +17,26 @@ public class Sea extends JPanel implements  ActionListener {
     public void paint(Graphics g){
         g = (Graphics2D) g;
         g.drawImage(img, f.sea1, 0, null);
+        g.drawImage(img, f.sea2, 0, null);
         g.drawImage(f.fish,f.x,f.y,null);
     }
 
     public Sea() {
         time.start();
+        addKeyListener(new myKeyAdapter());
+        setFocusable(true);
     }
+    private  class myKeyAdapter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e){
+            f.keyPressed(e);
+        }
+
+        public void keyReleased(KeyEvent e){
+            f.keyReleased(e);
+        }
+    }
+
     public void actionPerformed(ActionEvent e) {
         f.move();
         repaint();
